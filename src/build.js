6bd7417316c325fe02d3124a7f2963b0828ef0a0
MattIPv4 <me@mattcowley.co.uk>
2022-08-01 23:48:07 +0100

    YouTube video modal

--- src/build.js
+++ src/build.js
@@ -4,22 +4,33 @@ import { posthtmlInsertAt } from 'posthtml-insert-at';
 import postcss from 'postcss';
 import postcssUrl from 'postcss-url';
 import sass from 'sass';
+import esbuild from 'esbuild';
 import fs from 'fs/promises';
 import { fileURLToPath } from 'url';
 
 const main = async () => {
     // Compile our SCSS w/ sass, and inline the images w/ postcss-url
-    const styleSource = fileURLToPath(new URL('./style.scss', import.meta.url));
+    const styleSource = fileURLToPath(new URL('./index.scss', import.meta.url));
     const style = await sass.compileAsync(styleSource, { style: 'compressed' })
         .then(({ css }) => postcss()
             .use(postcssUrl({ url: 'inline', encodeType: 'base64' }))
             .process(css, { from: styleSource }));
 
-    // Compile our HTML w/ posthtml, and insert the style w/ posthtml-insert-at
+    // Compile our JS w/ esbuild
+    const jsSource = fileURLToPath(new URL('./index.js', import.meta.url));
+    const js = await esbuild.build({
+        entryPoints: [ jsSource ],
+        bundle: true,
+        write: false,
+        minify: true,
+    }).then(({ outputFiles }) => outputFiles.map(({ text }) => text).join('\n'));
+
+    // Compile our HTML w/ posthtml, and insert the JS + styles w/ posthtml-insert-at
     const htmlSource = await fs.readFile(new URL('./index.html', import.meta.url), 'utf8');
     const html = await posthtml()
         .use(posthtmlInclude({ root: fileURLToPath(new URL('./', import.meta.url)) }))
         .use(posthtmlInsertAt({ selector: 'head', append: `<style>${style.css}</style>` }))
+        .use(posthtmlInsertAt({ selector: 'body', append: `<script>${js}</script>` }))
         .process(htmlSource);
 
     // Output the HTML
---
+++ src/index.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Find all the links for YouTube videos
+    const links = document.querySelectorAll('a[href^="https://www.youtube.com/watch?v="],a[href^="https://youtu.be/"]');
+    for (const link of links) {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            const modal = document.createElement('dialog');
+
+            // Allow the user to close the modal via the background
+            const close = event => {
+                event.preventDefault();
+                modal.close();
+                modal.remove();
+            };
+            modal.addEventListener('click', close);
+
+            // Allow the user to close the modal via button
+            const button = document.createElement('button');
+            button.addEventListener('click', close);
+            modal.appendChild(button);
+
+            // Allow the user to close the modal via escape
+            const escape = event => {
+                if (event.key !== 'Escape') return;
+                close(event);
+                document.removeEventListener('keydown', escape);
+            };
+            document.addEventListener('keydown', escape);
+
+            // Inject the YouTube video
+            const iframe = document.createElement('iframe');
+            const videoId = link.href.match(/^https:\/\/(?:www\.youtube\.com\/watch\?v=|youtu\.be\/)(.+)$/)[1];
+            iframe.src = `https://www.youtube.com/embed/${videoId}`;
+            modal.appendChild(iframe);
+
+            document.body.appendChild(modal);
+            modal.show();
+        });
+    }
+});
